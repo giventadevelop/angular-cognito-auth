fd9ed4bf6cb85b5809c601736fe6d16e7c34ee11
giventadevelop <giventa.develop@gmail.com>
2025-01-15 12:42:53 -0500

    feat: remove unused AuthCustomModule from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthCustomModule } from './auth.module';
+
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthCustomModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
