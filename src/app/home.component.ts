c2e4171b0ef105b7a2a381b18f8955808d7a8996
giventadevelop <giventa.develop@gmail.com>
2025-01-15 14:51:13 -0500

    fix: Update login check condition in home component

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -20,7 +20,7 @@ interface UserData {
         <p>Authenticating...</p>
       </div>
       <ng-container *ngIf="!isLoading">
-        <div *ngIf="isLoggedIn && userDetails?.sub">
+        <div *ngIf="isLoggedIn || userDetails?.['email']">
           <h1>User logged in successfully!</h1>
           <button class="btn btn-secondary" (click)="logout()">
             <span class="btn-text">Logout</span>
